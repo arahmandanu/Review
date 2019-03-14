d9523edc028f14e8f5a69eed0330e3c346ce7d8c
Adrian <adrian.rahmandanu2@gmail.com>
2019-03-14 16:05:07 +0700

    first commit

---
+++ app/src/main/java/com/example/reviewtes/PojoData.java
@@ -0,0 +1,207 @@
+package com.example.reviewtes;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class PojoData {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("data")
+    @Expose
+    private Object data;
+    @SerializedName("dataMode")
+    @Expose
+    private Object dataMode;
+    @SerializedName("headerData")
+    @Expose
+    private Object headerData;
+    @SerializedName("method")
+    @Expose
+    private String method;
+    @SerializedName("pathVariableData")
+    @Expose
+    private List<Object> pathVariableData = null;
+    @SerializedName("queryParams")
+    @Expose
+    private List<Object> queryParams = null;
+    @SerializedName("auth")
+    @Expose
+    private Object auth;
+    @SerializedName("events")
+    @Expose
+    private Object events;
+    @SerializedName("folder")
+    @Expose
+    private Object folder;
+    @SerializedName("currentHelper")
+    @Expose
+    private Object currentHelper;
+    @SerializedName("helperAttributes")
+    @Expose
+    private Object helperAttributes;
+    @SerializedName("collectionId")
+    @Expose
+    private String collectionId;
+    @SerializedName("pathVariables")
+    @Expose
+    private List<Object> pathVariables = null;
+    @SerializedName("headers")
+    @Expose
+    private String headers;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Object getDataMode() {
+        return dataMode;
+    }
+
+    public void setDataMode(Object dataMode) {
+        this.dataMode = dataMode;
+    }
+
+    public Object getHeaderData() {
+        return headerData;
+    }
+
+    public void setHeaderData(Object headerData) {
+        this.headerData = headerData;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public List<Object> getPathVariableData() {
+        return pathVariableData;
+    }
+
+    public void setPathVariableData(List<Object> pathVariableData) {
+        this.pathVariableData = pathVariableData;
+    }
+
+    public List<Object> getQueryParams() {
+        return queryParams;
+    }
+
+    public void setQueryParams(List<Object> queryParams) {
+        this.queryParams = queryParams;
+    }
+
+    public Object getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Object auth) {
+        this.auth = auth;
+    }
+
+    public Object getEvents() {
+        return events;
+    }
+
+    public void setEvents(Object events) {
+        this.events = events;
+    }
+
+    public Object getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Object folder) {
+        this.folder = folder;
+    }
+
+    public Object getCurrentHelper() {
+        return currentHelper;
+    }
+
+    public void setCurrentHelper(Object currentHelper) {
+        this.currentHelper = currentHelper;
+    }
+
+    public Object getHelperAttributes() {
+        return helperAttributes;
+    }
+
+    public void setHelperAttributes(Object helperAttributes) {
+        this.helperAttributes = helperAttributes;
+    }
+
+    public String getCollectionId() {
+        return collectionId;
+    }
+
+    public void setCollectionId(String collectionId) {
+        this.collectionId = collectionId;
+    }
+
+    public List<Object> getPathVariables() {
+        return pathVariables;
+    }
+
+    public void setPathVariables(List<Object> pathVariables) {
+        this.pathVariables = pathVariables;
+    }
+
+    public String getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(String headers) {
+        this.headers = headers;
+    }
+}
---
+++ app/src/main/java/com/example/reviewtes/ResponData.java
@@ -0,0 +1,119 @@
+package com.example.reviewtes;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ResponData {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("auth")
+    @Expose
+    private Object auth;
+    @SerializedName("events")
+    @Expose
+    private Object events;
+    @SerializedName("variables")
+    @Expose
+    private List<Object> variables = null;
+    @SerializedName("order")
+    @Expose
+    private List<String> order = null;
+    @SerializedName("folders_order")
+    @Expose
+    private List<Object> foldersOrder = null;
+    @SerializedName("folders")
+    @Expose
+    private List<Object> folders = null;
+    @SerializedName("requests")
+    @Expose
+    private List<PojoData> requests = null;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getAuth() {
+        return auth;
+    }
+
+    public void setAuth(Object auth) {
+        this.auth = auth;
+    }
+
+    public Object getEvents() {
+        return events;
+    }
+
+    public void setEvents(Object events) {
+        this.events = events;
+    }
+
+    public List<Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(List<Object> variables) {
+        this.variables = variables;
+    }
+
+    public List<String> getOrder() {
+        return order;
+    }
+
+    public void setOrder(List<String> order) {
+        this.order = order;
+    }
+
+    public List<Object> getFoldersOrder() {
+        return foldersOrder;
+    }
+
+    public void setFoldersOrder(List<Object> foldersOrder) {
+        this.foldersOrder = foldersOrder;
+    }
+
+    public List<Object> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Object> folders) {
+        this.folders = folders;
+    }
+
+    public List<PojoData> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<PojoData> requests) {
+        this.requests = requests;
+    }
+}
